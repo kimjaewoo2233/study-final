cb1121103f032e62d3bbc1b83e1ccb19a41c0977
jaewoo2233 <ta3844@naver.com>
2023-05-22 21:56:11 +0900

    HOTFIX:teqqq

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-BUILD_JAR=$(ls /home/ubuntu/deploy/deploy/*.jar)
+BUILD_JAR=$(ls /home/ubuntu/deploy/deploy/demo-0.0.1-SNAPSHOT.jar)
 nohup java -jar "$BUILD_JAR" >> /home/ubuntu/deploy_test.log 2>/home/ubuntu/action/deploy_err_test.log &
