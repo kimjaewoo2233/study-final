63aff0afe8ff2f9073c71943c11bbbc03599fa5a
jaewoo2233 <ta3844@naver.com>
2023-05-22 12:57:26 +0900

    HOTFIX:배포에러 해결중

--- src/main/java/com/example/demo/api/TokenDTO.java
+++ src/main/java/com/example/demo/api/TokenDTO.java
@@ -8,6 +8,8 @@ import lombok.ToString;
 public class TokenDTO {
     private String access_token;
     private String token_type;
+
+
     private String refresh_token;
     private int expires_in;
     private String scope;
