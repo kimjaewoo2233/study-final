f86efa8f6000caf7bd6f0bf5b1d86b2b6d954195
jaewoo2233 <ta3844@naver.com>
2023-05-22 21:49:20 +0900

    HOTFIX:ㅁadd

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-BUILD_JAR=$(ls ./deploy/*.jar)
+BUILD_JAR=$(ls /home/ubuntu/deploy/deploy/*.jar)
 nohup java -jar "$BUILD_JAR" >> /home/ubuntu/deploy_test.log 2>/home/ubuntu/action/deploy_err_test.log &
